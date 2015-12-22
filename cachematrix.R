9deac4ae6fd46e73eba96d746789a734c1135711
zhoumy66 <zhoumy66@gmail.com>
2015-12-21 22:06:28 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions abced
+## If the inverse of the matrix have already been calculated, the R program will not repeat the calculation
+## but get the result of the inverse of the matrix from cache
 
-## Write a short comment describing this function
+## makeCacheMatrix function create a special Matrix that is a list to store the following 4 things: 
+## 1. set the value of the matrix 
+## 2. get the value of the matrix
+## 3. set the inverse matrix
+## 4. get the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s<-NULL
+        set<-function(y){
+                x<<-y
+                s<<-NULL
+                }
+        get<-function() x
+        setsolve<-function(solve) s<<-solve
+        getsolve<-function() s
+        list(set=set,get=get,
+                setsolve=setsolve,
+                getsolve=getsolve)
 }
 
 
